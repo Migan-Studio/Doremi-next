a826d9654eb91ccaab7f9a78908afd0875a87c8d
Migan178 <migan9857@gmail.com>
2023-09-30 12:11:33 +0900

    feat: guild information

--- src/interactions/information.ts
+++ src/interactions/information.ts
@@ -1,8 +1,14 @@
 import { getInformation, localzation } from '@localization'
-import { type StringSelectMenuInteraction } from 'discord.js'
+import {
+  GuildVerificationLevel,
+  Locale,
+  type StringSelectMenuInteraction,
+} from 'discord.js'
 import { arch, platform } from 'node:os'
 
-export function informationSelect(interaction: StringSelectMenuInteraction) {
+export function informationSelect(
+  interaction: StringSelectMenuInteraction<'cached'>,
+) {
   const locale = localzation(interaction.locale)
 
   switch (interaction.values[0]) {
@@ -34,6 +40,81 @@ export function informationSelect(interaction: StringSelectMenuInteraction) {
             }),
             color: interaction.client.COLOR,
             timestamp: new Date().toISOString(),
+          },
+        ],
+      })
+      break
+
+    case 'Doremi-information$guild':
+      function getGuildSecurity() {
+        if (interaction.locale === Locale.Korean) {
+          switch (interaction.guild.verificationLevel) {
+            case GuildVerificationLevel.High:
+              return '높음'
+            case GuildVerificationLevel.Low:
+              return '낮음'
+            case GuildVerificationLevel.Medium:
+              return '중간'
+            case GuildVerificationLevel.None:
+              return '없음'
+            case GuildVerificationLevel.VeryHigh:
+              return '매우 높음'
+          }
+        } else {
+          switch (interaction.guild.verificationLevel) {
+            case GuildVerificationLevel.High:
+              return 'High'
+            case GuildVerificationLevel.Low:
+              return 'Low'
+            case GuildVerificationLevel.Medium:
+              return 'Medium'
+            case GuildVerificationLevel.None:
+              return 'None'
+            case GuildVerificationLevel.VeryHigh:
+              return 'Very High'
+          }
+        }
+      }
+
+      let bot = 0
+      let user = 0
+      interaction.guild!.members.cache.forEach(member => {
+        if (member.user.bot) {
+          bot++
+        } else {
+          user++
+        }
+      })
+
+      interaction.update({
+        embeds: [
+          {
+            title: locale.information.embeds.title.replace(
+              '{name}',
+              interaction.guild.name,
+            ),
+            description: getInformation(interaction.locale).guild({
+              name: interaction.guild.name,
+              owner: {
+                name: interaction.guild.members.cache.get(
+                  interaction.guild.ownerId,
+                )!.user.username,
+                id: interaction.guild.ownerId,
+              },
+              count: {
+                boost: interaction.guild.premiumSubscriptionCount || 0,
+                member: interaction.guild.memberCount,
+                bot,
+                memberOnly: user,
+                emoji: interaction.guild.emojis.cache.size,
+                sticky: interaction.guild.stickers.cache.size,
+              },
+              security: getGuildSecurity(),
+            }),
+            timestamp: new Date().toISOString(),
+            thumbnail: {
+              url: interaction.guild.iconURL()!,
+            },
           },
         ],
       })
