4d733db026aa8dfb0e4d84832008d0a734455b46
Migan178 <migan9857@gmail.com>
2023-11-23 12:34:42 +0900

    feat: add table ticket

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,12 +1,24 @@
-CREATE TABLE server (
-    id int(19),
-    owner_id int(19),
-    primary key (`id`)
-);
+CREATE TABLE
+    server (
+        id INTEGER NOT NULL,
+        owner_id VARCHAR(255 NOT NULL),
+        PRIMARY KEY('id') NOT NULL
+    );
 
-CREATE TABLE warn (
-    id int(19),
-    warn_count int (9),
-    server_id int(19),
-    primary key (`id`)
-);
+CREATE TABLE
+    warn (
+        id INTEGER NOT NULL,
+        warn_count INTEGER NOT NULL,
+        server_id VARCHAR(255) NOT NULL,
+        PRIMARY KEY('id')
+    );
+
+CREATE TABLE
+    ticket (
+        id INTEGER NOT NULL,
+        user_id VARCHAR(255) NOT NULL,
+        channel_id VARCHAR(255) NOT NULL,
+        closed BOOLEAN NOT NULL DEFAULT 0,
+        create_at DATETIME NOT NULL DEFAULT current_timestamp(),
+        PRIMARY KEY('id')
+    );
