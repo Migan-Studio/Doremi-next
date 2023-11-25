9839de4368dc749b219647601c3e20400c8af6f8
Migan178 <migan9857@gmail.com>
2023-11-25 23:03:33 +0900

    feat: ,

--- src/utils/database.ts
+++ src/utils/database.ts
@@ -43,6 +43,50 @@ class GuildTable {
   }
 }
 
+class WarnTable {
+  public constructor(private _database: Pool) {}
+
+  public async all() {
+    return await this._database.execute<GuildData[]>(`select * from warn;`)
+  }
+
+  public async insert(data: {
+    warn_count: number
+    guild_id: Snowflake
+    user_id: Snowflake
+  }) {
+    const db = await this._database.getConnection()
+
+    try {
+      await db.beginTransaction()
+      await db.execute(
+        `insert into warn (warn_count, guild_id, user_id) values (?, ?, ?);`,
+        [data.warn_count, data.guild_id, data.user_id],
+      )
+      await db.commit()
+    } catch (err) {
+      console.error(err)
+      await db.rollback()
+    } finally {
+      db.release()
+    }
+  }
+
+  public async delete(key: Snowflake) {
+    const db = await this._database.getConnection()
+
+    try {
+      await db.beginTransaction()
+      await db.execute(`delete from warn where guild_id = ?;`, [key])
+      await db.commit()
+    } catch (err) {
+      console.error(err)
+      await db.rollback()
+    } finally {
+      db.release()
+    }
+  }
+}
 export class Database {
   private _database = createPool({
     ...config.mysql,
@@ -51,8 +95,13 @@ export class Database {
   })
 
   private _guild = new GuildTable(this._database)
+  private _warn = new WarnTable(this._database)
 
   public get guild() {
     return this._guild
+  }
+
+  public get warn() {
+    return this._warn
   }
 }
