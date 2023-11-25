97e83ca32a092d845b183333e8725759e69a4eab
Migan178 <migan9857@gmail.com>
2023-11-25 21:07:08 +0900

    asdf

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -2,6 +2,7 @@ create table
     guild (
         id integer not null,
         owner_id varchar(255) not null,
+        guild_id varchar(255) not null,
         primary key('id')
     );
 
@@ -9,7 +10,7 @@ create table
     warn (
         id integer not null,
         warn_count integer not null,
-        server_id varchar(255) not null,
+        guild_id varchar(255) not null,
         user_id varchar(255) not null,
         primary key('id')
     );
@@ -20,6 +21,7 @@ create table
         user_id varchar(255) not null,
         channel_id varchar(255) not null,
         closed boolean not null default 0,
+        guild_id varchar(255) not null,
         create_at datetime not null default current_timestamp(),
         primary key('id')
     );
